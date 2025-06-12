dd74a4b8c654a24970e67317d0b01106a1ef2334
oxilunna <lunnasousafe@gmail.com>
2025-06-12 14:29:23 -0300

    arrumando banco e testando função

--- TrabalhoDPSTADW/controle/tests/test_salvarUsuario.php
+++ TrabalhoDPSTADW/controle/tests/test_salvarUsuario.php
@@ -2,8 +2,8 @@
     require_once "../conexao.php";
     require_once "../funcoes.php";
 
-    $nome = "Kaio";
-    $gmail = "Kaio@teste.com";
+    $nome = "Lunna";
+    $gmail = "Lunna@teste.com";
     $senha = "3840413708";
 
     salvarUsuario($conexao, $nome, $gmail, $senha);
