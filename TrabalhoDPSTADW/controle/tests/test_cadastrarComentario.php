6308689feacd0979edebfa67d336d84fb991bde6
Paulin09k2 <lunnasousafe@gmail.com>
2025-08-21 16:26:14 -0300

    arrumando

--- TrabalhoDPSTADW/controle/tests/test_cadastrarComentario.php
+++ TrabalhoDPSTADW/controle/tests/test_cadastrarComentario.php
@@ -4,9 +4,9 @@
 
     $comentario = " Que jogo lixo!!!!";
     $criado = "2025-08-12";
-    $post_forun_idpost_forun = "";
-    $post_forun_usuario_idusuario = "";
-    $post_forun_topico_forun_idtopico_forun = "";
+    $post_forun_idpost_forun = 1;
+    $post_forun_usuario_idusuario = 2;
+    $post_forun_topico_forun_idtopico_forun = 1;
     
 
     cadastrarComentario($conexao, $comentario, $criado, $post_forun_idpost_forun, $post_forun_usuario_idusuario, $post_forun_topico_forun_idtopico_forun);
