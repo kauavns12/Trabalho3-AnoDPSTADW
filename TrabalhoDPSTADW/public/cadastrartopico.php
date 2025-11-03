1b2b9ae2fe084c4b5b2383c9c2a7600a49321132
kauavns12 <kauavitornunessantos12@gmail.com>
2025-11-03 16:15:17 -0300

    CSS de 90 dos Posts

--- TrabalhoDPSTADW/public/cadastrartopico.php
+++ TrabalhoDPSTADW/public/cadastrartopico.php
@@ -12,7 +12,8 @@ require_once "../controle/funcoes.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu - FRIV GAMES & WIKI</title>
-    <link rel="stylesheet" href="./estilo/stilinho.css">
+    <link rel="stylesheet" href="./estilo/estilo_cadastrarTopico.css">
+    <link rel="stylesheet" href="./estilo/cabeçalho.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 
