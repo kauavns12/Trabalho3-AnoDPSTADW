7dc1a39d2481131e49b426f088978375d0e01dcc
kauavns12 <kauavitornunessantos12@gmail.com>
2025-11-06 08:30:38 -0300

    Excluir do Jogos

--- TrabalhoDPSTADW/public/comunidade.php
+++ TrabalhoDPSTADW/public/comunidade.php
@@ -11,49 +11,60 @@ require_once "../controle/funcoes.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu - FRIV GAMES & WIKI</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="./estilo/estilo_comunidade.css">
     <link rel="stylesheet" href="./estilo/cabeçalho.css">
-
-     <style>
-        img {
-            width: 50px;
-            height: 50px;
-        }
-    </style>
 </head>
 
 <body>
-    
- <?php include 'cabeçalho.php'; ?>
-    <br><br><br>
+    <?php include 'cabeçalho.php'; ?>
 
-    <?php
-
+    <div class="page-container">
+        <h1 class="page-title">Comunidade</h1>
+        
+        <?php
         $usuarios = listarUsuario($conexao);
 
-    if (count($usuarios) == 0) {
-        echo "Não existem usuários cadastrados";
-    } else {
-    ?>
-        <table border="1">
-            <tr>
-                <td>FOTO</td>
-                <td>NOME</td>
-                <td colspan="2">Ação</td>
-            </tr>
-        <?php
-        foreach ($usuarios as $usuario) {
-            $idusuario = $usuario['idusuario'];
-            $nome = $usuario['nome'];
-            $foto = $usuario['foto'];
+        if (count($usuarios) == 0) {
+            echo "<div class='empty-state'>Não existem usuários cadastrados</div>";
+        } else {
+        ?>
+            <div class="table-container">
+                <table class="users-table">
+                    <thead>
+                        <tr>
+                            <th>FOTO</th>
+                            <th>NOME</th>
+                            <th>AÇÃO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($usuarios as $usuario) {
+                        $idusuario = $usuario['idusuario'];
+                        $nome = $usuario['nome'];
+                        $foto = $usuario['foto'];
 
-            echo "<tr>";
-            echo "<td><img src='../controle/fotos/$foto'></td>";
-            echo "<td>$nome</td>";
-            echo "<td><a href='usuario.php?id=$idusuario'>Visualizar perfil</a></td>";
-            echo "</tr>";
-        }
-    }
+                        // Verificar se a foto existe
+                        $caminhoFoto = "../controle/fotos/$foto";
+                        $fotoPadrao = "../controle/fotos/default.png";
 
-    ?>
+                        if (!file_exists($caminhoFoto) || empty($foto)) {
+                            $caminhoFoto = $fotoPadrao;
+                        }
 
-
+                        echo "<tr>";
+                        echo "<td><div class='user-avatar-container'><img src='$caminhoFoto' alt='$nome' class='user-avatar' onerror=\"this.src='$fotoPadrao'\"></div></td>";
+                        echo "<td>$nome</td>";
+                        echo "<td><a href='usuario.php?id=$idusuario' class='action-link'><i class='fas fa-user'></i> Visualizar Perfil</a></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php } ?>
+        
+        <a href='home.php' class='back-button'>Voltar para Home</a>
+    </div>
+</body>
+</html>
