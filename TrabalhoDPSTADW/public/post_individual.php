1a98cb37636c6479ee598ef0c06ee303561443d9
kuan <kauavitornunessantos12@gmail.com>
2025-11-10 17:30:47 -0300

    Css do post individual

--- TrabalhoDPSTADW/public/post_individual.php
+++ TrabalhoDPSTADW/public/post_individual.php
@@ -10,102 +10,104 @@ $idpost = $_GET['id'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu - FRIV GAMES & WIKI</title>
+    <title>Post - FRIV GAMES & WIKI</title>
     <link rel="stylesheet" href="./estilo/estilo_postIndividual.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="./estilo/cabeçalho.css">
-
-
 </head>
 
 <body>
     
     <?php include 'cabeçalho.php'; ?>
-    <br><br><br>
 
-    <?php
+    <div class="post-container">
+        <div class="post-header">
+            <h1 class="post-title">Post da Comunidade</h1>
+        </div>
 
-        // echo "<pre>";
-        // print_r(pesquisarPostID($conexao, $idpost));
-        // echo "</pre>";
-        
-        // echo "<pre>";
-        // print_r(ListarComentarioPost($conexao, $idpost));
-        // echo "</pre>";
+        <?php
         $posts = [];
         $posts[] = pesquisarPostID($conexao, $idpost);
         
         if((!$posts)){
-            echo "Não foi possível identificar o post. Retorne a página e tente rever o post.";
+            echo "<div class='empty-message'>Não foi possível identificar o post. Retorne a página e tente rever o post.</div>";
         } else {
-
-            ?>
-
-        <table border="1">
-            <tr>
-                <td>Usuário</td>
-                <td>Foto</td>
-                <td>Tópico</td>
-                <td>Conteúdo</td>
-            </tr>
-        <?php
-        foreach ($posts as $post) {
-
-            $idp_forum = $post['idpost_forun'];
-            $conteudo = $post['conteudo'];
-            $idusu = $post['usuario_idusuario'];
-            $idtop = $post['topico_forun_idtopico_forun'];
-
-            $usuarioinfo = pesquisarUsuario_ID($conexao, $idusu);
-            $nome = $usuarioinfo['nome'];
-            $foto = $usuarioinfo['foto'];
-            
-            $topico_info = pesquisarTopico_ID($conexao, $idtop);
-            $nometopico = $topico_info['nome_topico'];
-
-
-            echo "<tr>";
-            echo "<td>$nome</td>";
-            echo "<td><img src='../controle/fotos/$foto'></td>";
-            echo "<td>$nometopico</td>";
-            echo "<td>$conteudo</td>";
-            echo "</tr>";
-        }
-    }
         ?>
-        </table>
+        
+        <div class="post-content">
+            <table class="post-table">
+                <thead>
+                    <tr>
+                        <th>Usuário</th>
+                        <th>Foto</th>
+                        <th>Tópico</th>
+                        <th>Conteúdo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($posts as $post) {
+                    $idp_forum = $post['idpost_forun'];
+                    $conteudo = $post['conteudo'];
+                    $idusu = $post['usuario_idusuario'];
+                    $idtop = $post['topico_forun_idtopico_forun'];
+
+                    $usuarioinfo = pesquisarUsuario_ID($conexao, $idusu);
+                    $nome = $usuarioinfo['nome'];
+                    $foto = $usuarioinfo['foto'];
+                    
+                    $topico_info = pesquisarTopico_ID($conexao, $idtop);
+                    $nometopico = $topico_info['nome_topico'];
+
+                    echo "<tr>";
+                    echo "<td><strong>$nome</strong></td>";
+                    echo "<td><img src='../controle/fotos/$foto' alt='$nome' class='user-avatar'></td>";
+                    echo "<td><span class='topic-badge'>$nometopico</span></td>";
+                    echo "<td class='post-text'>$conteudo</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
 
         <?php 
-        
         $comentarios = ListarComentarioPost($conexao, $idpost);
-        
-        if(count($comentarios)==0){
-            echo "Não foi possível encontrar os comentarios do post. Recarregue a página ou solicite uma manutenção.";
-        } else {
-
-            ?>
-
-        <table border="1">
-            <tr>
-                <td>Comentário</td>
-            </tr>
-        <?php
-        foreach ($comentarios as $comentario) {
-
-
-            $comen = $comentario['comentario'];
-
-
-            echo "<tr>";
-            echo "<td>$comen</td>";
-            echo "</tr>";
-        }
-    }
         ?>
-        </table>
-         
         
+        <div class="comments-section">
+            <h2 class="comments-title">Comentários</h2>
+            
+            <?php
+            if(count($comentarios)==0){
+                echo "<div class='empty-message'>Não há comentários neste post ainda.</div>";
+            } else {
+            ?>
+            
+            <table class="comments-table">
+                <thead>
+                    <tr>
+                        <th>Comentário</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($comentarios as $comentario) {
+                    $comen = $comentario['comentario'];
+                    echo "<tr>";
+                    echo "<td>$comen</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+            <?php } ?>
+        </div>
+        
+        <?php } ?>
+        
+        <a href="comunidade.php" class="back-button">Voltar para Comunidade</a>
+    </div>
+         
 </body>
-
-
-    
+</html>
